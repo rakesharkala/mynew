4fba99a4d71da10d04b47e54ff533419d2a15de8
KC278HZ <rakesharkala001@gmail.com>
2026-05-11 15:54:53 +0530

    changes are added to and created a method inside it

--- src/FirstJava.java
+++ src/FirstJava.java
@@ -2,5 +2,10 @@ public class FirstJava {
     public static void main(String args[])
     {
         System.out.println("my first story");
+        FirstJava.ramesh();
     }
+    public static void ramesh(){
+        System.out.println("called ");
+    }
+
 }
